19aca972ce55b8f3a6a9b0892a253c08bcf5f900
Nuray <nuray.kavkaeva.04@gmail.com>
2024-01-01 14:39:16 +0600

    first commit

---
+++ src/main/java/com/example/careerconnecthub/CareerConnectHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.careerconnecthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerConnectHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerConnectHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/careerconnecthub/CareerConnectHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.careerconnecthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerConnectHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
